511421bd2c98d26ea97ffabb5f03b3822e81673f
fredvs <fiens@hotmail.com>
2020-10-27 12:23:56 +0100

    Added ALSAbeepStereo().

--- src/alsa_sound.pas
+++ src/alsa_sound.pas
@@ -82,7 +82,10 @@ procedure as_Unload();     // unload and frees the lib from memory : do not forg
 
 function ALSAbeep(frequency, duration, volume: integer; warble: Boolean; CloseLib : boolean): Boolean;
 
+function ALSAbeepStereo(Frequency1, Frequency2, Duration, Volume1, Volume2: integer; warble: Boolean; CloseLib : boolean): Boolean;
+
 function ALSAglide(StartFreq,EndFreq, duration, volume: integer; CloseLib : boolean): Boolean;
+
 
 implementation
 
@@ -245,12 +248,13 @@ end;
   
 function ALSAbeep(frequency, duration, volume: integer; warble: Boolean; CloseLib : boolean): Boolean;
 var
-  buffer: array[0..9600 - 1] of byte;  // 1/5th second worth of samples @48000Hz
-  frames: snd_pcm_sframes_t;           // number of frames written (negative if an error occurred)
+  buffer: array[0..(9600) - 1] of byte;  // 1/5th second worth of samples @48000Hz
+ frames: snd_pcm_sframes_t;           // number of frames written (negative if an error occurred)
   pcm: PPsnd_pcm_t;                    // sound device handle
   I, FC: integer;
   SA: array[0..359] of shortint;       // array of sine wave values for a single cycle
-const
+
+ const
   device = 'default' + #0;             // name of sound device
 var
   count1, count2, N, X, Y: integer;
@@ -278,7 +282,7 @@ begin
         duration := 50;         //   |-- restrict parameters to usable ranges
       if volume > 100 then
         volume   := 100;        // -/
-
+     
       for I := 0 to 359 do
         SA[I] := round(sin(pi * I / 180.0) * volume);  // create sine wave pattern
       X       := 0;
@@ -322,6 +326,140 @@ begin
 
           Inc(count1);
           Dec(count2);
+        end; 
+        
+        if FC <= 2400 then Y := 2400 else Y := FC;
+
+        frames   := snd_pcm_writei(pcm, @buffer, Y); // write AT LEAST one full period
+    
+        if frames < 0 then
+          frames := snd_pcm_recover(pcm, frames, 0); // try to recover from any error
+        if frames < 0 then
+          break;                        // give up if failed to recover
+      end;
+      snd_pcm_drain(pcm);              // drain any remaining samples
+      snd_pcm_close(pcm);
+    end;
+   if CloseLib then as_unload;  // Unload library if param CloseLib is true
+end;
+
+function ALSAbeepStereo(Frequency1, Frequency2, Duration, Volume1, Volume2: integer; warble: Boolean; CloseLib : boolean): Boolean;
+var
+  buffer: array[0..(9600*2) - 1] of byte;  // 1/5th second worth of samples @48000Hz
+  frames: snd_pcm_sframes_t;           // number of frames written (negative if an error occurred)
+  pcm: PPsnd_pcm_t;                    // sound device handle
+  I, FC: integer;
+  SA, SA2: array[0..359] of shortint;       // array of sine wave values for a single cycle
+const
+  device = 'default' + #0;             // name of sound device
+var
+  count1, count2, N, N2, X, X2, Y: integer;
+begin
+  Result := False;
+
+  as_Load;       // load the library
+
+  if snd_pcm_open(@pcm, @device[1], SND_PCM_STREAM_PLAYBACK, 0) = 0 then
+    if snd_pcm_set_params(pcm, SND_PCM_FORMAT_U8,
+      SND_PCM_ACCESS_RW_INTERLEAVED,
+      2,                        // number of channels
+      48000,                    // sample rate (Hz)
+      1,                        // resampling on/off
+      500000) = 0 then            // latency (us)
+    begin
+      Result := True;
+  
+      frequency1 := abs(frequency1);  // -\
+      frequency2 := abs(frequency2);
+      duration  := abs(duration);     //   |-- ensure no parameters are negative
+      volume1    := abs(volume1);     
+      volume2    := abs(volume2);     // -/
+      if frequency1 < 20 then
+        frequency1 := 20;        // -\
+      if frequency2 < 20 then
+        frequency2 := 20;        // -  
+      if duration < 50 then
+        duration := 50;         //   |-- restrict parameters to usable ranges
+      if volume1 > 100 then
+        volume1   := 100;        // -/  
+      if volume2 > 100 then
+        volume2   := 100;        // -/  
+  
+
+      for I := 0 to 359 do
+        SA[I] := round(sin(pi * I / 180.0) * volume1);  // create sine wave pattern
+        
+      for I := 0 to 359 do
+        SA2[I] := round(sin(pi * I / 180.0) * volume2);  // create sine wave pattern
+                          
+      X       := 0;
+      N       := 0;       // up/down counter used by unequal interval division
+                             
+      X2       := 0;   // stereo
+      N2       := 0; 
+
+      count1 := 0;        // count1 counts up, count2 counts down
+      count2 := duration * 48;     // (at 48000Hz there are 48 samples per ms)
+
+      while count2 > 0 do       // start making sound!
+      begin
+        FC    := 0;
+        I    := 0;
+        while I < sizeof(buffer) do
+        begin
+          if count2 > 0 then
+          begin
+            if count1 < 480 then
+            begin
+           buffer[I] := 128 + ((count1 * SA[X]) div 480);
+           buffer[I+1] := 128 + ((count1 * SA2[X2]) div 480);
+            end  
+            else   // 10ms feather in
+            if count2 < 480 then
+            begin
+            buffer[I] := 128 + ((count2 * SA[X]) div 480);
+            buffer[I+1] := 128 + ((count2 * SA2[X2]) div 480);
+            end  
+            else   // 10ms feather out
+            begin
+              buffer[I] := 128 + (SA[X]);
+              buffer[I+1] := 128 + (SA2[X2]);
+            end;  
+            if warble and odd(count1 div 120) then
+            begin
+            buffer[I] := 128;              // 200Hz warble
+            buffer[I+1] := 128;
+            end;
+            Inc(FC);
+          end
+          else
+          begin
+            buffer[I] := 128;  // no signal on trailing end of buffer, just in case
+            buffer[I+1] := 128;
+            if (FC mod 2400) <> 0 then
+              Inc(FC);        // keep increasing FC until is a multiple of 2400
+          end;
+
+          Inc(N, frequency1 * 360); // unequal interval division routine
+          while (N > 0) do
+          begin                    // (a variation on Bresenham's Algorithm)
+            Dec(N, 48000);
+            Inc(X);
+          end;
+          X := X mod 360;
+          
+           Inc(N2, (frequency2 div 2) * 360); // unequal interval division routine
+          while (N2 > 0) do
+          begin                    // (a variation on Bresenham's Algorithm)
+            Dec(N2, 48000);
+            Inc(X2);
+          end;
+          X2 := X2 mod 360;
+
+          Inc(count1);
+          Dec(count2);
+          
+          inc(I,2);
         end; 
         
         if FC <= 2400 then Y := 2400 else Y := FC;
--- src/alsa_sound_test.pas
+++ src/alsa_sound_test.pas
@@ -6,12 +6,15 @@ alsa_sound;
 begin
 { function ALSAglide(StartFreq, EndFreq, Duration, Volume: integer;
   CloseLib : boolean): Boolean; }
-  
 ALSAglide(20, 880, 500, 75, false); 
 ALSAglide(880, 20, 500, 75, false); 
 
 { function ALSAbeep(Frequency, Duration, Volume: integer; Warble: Boolean; 
   CloseLib: boolean): Boolean; }
 ALSAbeep(880, 100, 75, false, false);   
-ALSAbeep(840, 100, 75, true, true); // CloseLib = true to close the library
+ALSAbeep(840, 100, 75, true, false);
+
+{ function ALSAbeepStereo(Frequency1, Frequency2, Duration,
+  Volume1, Volume2: integer; warble: Boolean; CloseLib : boolean): Boolean; }
+ALSAbeepStereo(440, 660, 1000, 75, 50, false, true); // CloseLib = true to close the library
 end.
